22fcd03da396fcd7147530264e964714aa5c0cc1
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2025-07-10 14:38:50 +0200

    Slightly shitty resizing

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,38 @@
 import "tachyons";
 import "./index.css";
 import { Editor } from "./Editor.ts";
+import { makeResizableDiv } from "./Resizable.ts";
 
 const rootEl = document.querySelector("#root");
 if (rootEl) {
 	rootEl.innerHTML = `
-  <div id="console-app-container" class="console-app fl mh3 mv2 pt4">
-      <div class="shadow-3 br3">
-          <div id="window-titlebar" class="titlebar bg-near-black pv2 br3 br--top">
-              <div class="dib titlebar-buttons ml1 mr5 pl2">
-                  <span class="dib titlebar-button br-100 bg-red"></span>
-                  <span class="dib titlebar-button br-100 bg-yellow mh2"></span>
-                  <span class="dib titlebar-button br-100 bg-green"></span>
-              </div>
-          </div>
-          <div id="app-window" class="window bg-white pt3 light-silver h5 br2 br--bottom">
-              <div id="editor-window" class="window-content ph3 ma0 code"></div>
-          </div>
-      </div>
-  </div>
+<div id="console-app-container" class="console-app fl mh3 mv2">
+    <div class='resizers'>
+        <div class='resizer top-left'></div>
+        <div class='resizer top-right'></div>
+        <div class='resizer bottom-left'></div>
+        <div class='resizer bottom-right'></div>
+    </div>
+    <div id="shadow" class="shadow-3 br3">
+        <div id="window-titlebar" class="titlebar bg-near-black pv2 br3 br--top">
+            <div class="dib titlebar-buttons ml1 mr5 pl2">
+                <span class="dib titlebar-button br-100 bg-red"></span>
+                <span class="dib titlebar-button br-100 bg-yellow mh2"></span>
+                <span class="dib titlebar-button br-100 bg-green"></span>
+            </div>
+        </div>
+        <div id="app-window" class="window bg-white pt3 light-silver br2 br--bottom">
+            <div id="editor-window" class="window-content ph3 ma0 code"></div>
+        </div>
+    </div>
+</div>
 `;
 }
 
 const editor_window: HTMLDivElement | null =
 	document.querySelector("#editor-window");
+
+makeResizableDiv("#console-app-container");
 
 globalThis.editor = new Editor();
 
